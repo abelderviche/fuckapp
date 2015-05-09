4a9f9cb7aeb03c640d6d08dc007acb5a5cc0e369
Patricio <patricioarango@gmail.com>
2015-05-09 14:26:45 -0300

    los barbudos se la comen mucho

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,7 +68,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>ABEL - SI REEMPLAZAS ESTA PAGINA POR EL HTML DEL INDEX VA A SER EL INDEX</h1>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
