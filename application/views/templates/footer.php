fbe27618445a6144cc48907f9ed5a2163616c1d0
patricioarango <patricioarango@gmail.com>
2015-05-19 12:24:40 -0300

    abel puto atende el hangout

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,9 +1,9 @@
 </div> <!-- close div containder -->
 <script src="<?php echo asset_url(); ?>js/jquery-2.1.4.min.js" type="text/javascript"></script>
 <script src="<?php echo asset_url(); ?>js/materialize.min.js" type="text/javascript"></script>
-<script src="<?php echo asset_url(); ?>js/modernizr.min.js" type="text/javascript"></script>
-<script src="<?php echo asset_url(); ?>js/recorder.min.js" type="text/javascript"></script>
-<script src="<?php echo asset_url(); ?>js/recorderWorker.min.js" type="text/javascript"></script>
-<script src="<?php echo asset_url(); ?>js/speech.min.js" type="text/javascript"></script>
+<script src="<?php echo asset_url(); ?>js/modernizr.js" type="text/javascript"></script>
+<script src="<?php echo asset_url(); ?>js/recorder.js" type="text/javascript"></script>
+<script src="<?php echo asset_url(); ?>js/recorderWorker.js" type="text/javascript"></script>
+<script src="<?php echo asset_url(); ?>js/speech.js" type="text/javascript"></script>
 </body>
 </html>
