c942405be7340711ca31cdd35dee8e4df6157a61
Carlos <Carlos>
2015-12-11 18:02:38 -0300

    ayuda

---
+++ application/views/carta_ayuda.php
@@ -0,0 +1,7 @@
+
+<div id="container-m" class="ed-container" style="background:#e0e0e0;font-size:14px;margin-top:38px;">
+  <div class="ed-item" >
+   <figure alt="logo"><img src="<?php echo asset_url(); ?>/img/ayuda.png" alt=""/></figure>
+  </div>
+</div>
+  
